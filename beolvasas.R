d51920d94742c9eb3fb1afcbbab051ba7cf7ff2c
Kokeny_Gergely <teltemeto@gmail.com>
2023-01-24 17:42:36 +0100

    beolvasás for-ral

--- beolvasas.R
+++ beolvasas.R
@@ -1,6 +1,7 @@
 beolvasasok=dir("Data/")
 Tes4=read.csv(paste0("Data/",beolvasasok[1]),skip=1,header = FALSE)
-Tes4=rbind(Tes4,read.csv(paste0("Data/",beolvasasok[2]),skip=1,header = FALSE))
+for(nr in 2:5)
+  Tes4=rbind(Tes4,read.csv(paste0("Data/",beolvasasok[nr]),skip=1,header = FALSE))
 
 #idosor
 library(xts)
@@ -8,4 +9,3 @@ idő=as.POSIXct(Tes4[,2])
 Tes4.xts=xts(Tes4[,3:5], idő)
 plot(Tes4.xts)
 
-Tes4=rbind(Tes4,read.csv(paste0("Data/",beolvasasok[3]),skip=1,header = FALSE))
