b62d0805278796d8aa9d15ce1a0b3ecad694ea8f
Péter Kalicz <kaliczp@gmail.com>
2023-01-26 13:37:32 +0100

    For ciklus tetszőleges számú fájlhoz
    A length() függvény segítségével nem kell tudni, hogy hány fájlt
    olvasunk be.

--- beolvasas.R
+++ beolvasas.R
@@ -1,6 +1,6 @@
 beolvasasok=dir("Data/")
 Tes4=read.csv(paste0("Data/",beolvasasok[1]),skip=1,header = FALSE)
-for(nr in 2:5)
+for(nr in 2:length(beolvasasok))
   Tes4=rbind(Tes4,read.csv(paste0("Data/",beolvasasok[nr]),skip=1,header = FALSE))
 
 #idosor
